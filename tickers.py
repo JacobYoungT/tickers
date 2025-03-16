7170b48b3737da2d89a05d017304af976ff58013
JacobYoungT <jacob.schodt.thomsen@gmail.com>
2025-03-16 16:59:21 +0100

    Update tickers.py

--- tickers.py
+++ tickers.py
@@ -1,10 +1,10 @@
 import os
 import pandas as pd
 
-class Tickers:
+class FetchTickers:
     def __init__(self):
         self.sp500 = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies#S&P_500_component_stocks'
-        self.c25 = os.path.join(os.path.dirname(os.path.abspath(__file__)), "raw", "C25.xlsx")
+        self.c25 = 'https://en.wikipedia.org/wiki/OMX_Copenhagen_25#Components'
 
     def fetch_sp500_tickers(self):
         df = pd.read_html(self.sp500, header=0)[0]
@@ -13,8 +13,11 @@ class Tickers:
                .assign(Market='S&P 500'))
 
     def fetch_c25_tickers(self):
-        df = pd.read_excel(self.c25)
-        return df.assign(Market='C25')
+        df = pd.read_html(self.c25, header=0)[0]
+        return (df[['Ticker symbol', 'Company']]
+                .rename(columns={'Ticker symbol':'Ticker'})
+                .assign(Market='C25')
+                .assign(Ticker=lambda x: x['Ticker'].str.replace(' ', '-') + '.CO'))
 
     def output_tickers(self):
         sp500 = self.fetch_sp500_tickers()
@@ -24,6 +27,4 @@ class Tickers:
         df.to_excel(output_path, index=False)
         return print("Tickers has been exported.")
 
-
-scraper = Tickers()
-scraper.output_tickers()
+FetchTickers().output_tickers()
